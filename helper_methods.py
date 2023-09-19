eb4bd7efdc6405b28c7ee0011629f4192966df6f
Raul Martinez <raulmartinez1855@gmail.com>
2023-09-19 12:50:10 -0400

    update helper methods for db seeding

--- helper_methods.py
+++ helper_methods.py
@@ -6,12 +6,13 @@ import yfinance
 from statsmodels.tsa.stattools import adfuller, coint
 load_dotenv()
 DB_URL = os.getenv('DB_CONNECTION_STRING')
+EXPECTED_DF_LEN = 1006
 
 
 def get_stock_prices(ticker, start_date, end_date):
     # use date format YYYY-MM-DD
     prices_df = yfinance.Ticker(ticker).history(
-        start=start_date, end=end_date)
+        start=start_date, end=end_date, raise_errors=True)
     prices_df['Ticker'] = ticker
     return prices_df
 
@@ -58,7 +59,8 @@ def create_stock_price_table():
     sql_query = """
                 CREATE TABLE IF NOT EXISTS stock_prices(
                     id SERIAL PRIMARY KEY,
-                    ticker VARCHAR(10) NOT NULL,
+                    ticker VARCHAR(20) NOT NULL,
+                    sector VARCHAR(20) NOT NULL,
                     price FLOAT(8) NOT NULL,
                     date DATE NOT NULL,
                     UNIQUE(ticker, date)
@@ -68,23 +70,28 @@ def create_stock_price_table():
 
 
 def drop_stock_prices_table():
-    sql_query = """DROP TABLE stock_prices;"""
+    sql_query = """DROP TABLE IF EXISTS stock_prices;"""
     sql_execution_wrapper(sql_query)
 
 
-def insert_stock_records_to_db(ticker, start_date, end_date):
-    df = get_stock_prices(ticker, start_date, end_date)
-
-    df['sql_insert'] = df['Date'].dt.strftime('%Y-%m-%d')
-    df['sql_insert'] = '(' + round(df['Close'], 2).astype(str) + ',\'' + \
-        df['Ticker'] + '\',\'' + df['sql_insert'] + '\')'
-
-    insert_string = ',\n'.join(df['sql_insert'].array)
-    sql_query = f"""
-                INSERT INTO public.stock_prices(price, ticker, date)
-                VALUES {insert_string};
-                """
-    sql_execution_wrapper(sql_query)
+def insert_stock_records_to_db(ticker, sector, start_date, end_date):
+    try:
+        df = get_stock_prices(ticker, start_date, end_date).reset_index()
+        if (len(df) != EXPECTED_DF_LEN):
+            print(
+                f'{ticker} in sector {sector}, does not contain the expected number of rows: {len(df)}')
+        df['sql_insert'] = df['Date'].dt.strftime('%Y-%m-%d')
+        df['sql_insert'] = '(' + round(df['Close'], 2).astype(str) + ',\'' + \
+            df['Ticker'] + '\',\'' + df['sql_insert'] + '\',\'' + sector + '\')'
+        insert_string = ',\n'.join(df['sql_insert'].array)
+        sql_query = f"""
+                    INSERT INTO public.stock_prices(price, ticker, date, sector)
+                    VALUES {insert_string};
+                    """
+        sql_execution_wrapper(sql_query)
+    except Exception:
+        print(
+            f'Unable to find Stock Data for {ticker} in sector {sector}, skipping this entry')
 
 
 def get_stock_prices_from_db():
