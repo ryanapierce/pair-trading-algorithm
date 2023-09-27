fe05c4981ddf3e65877df8b41122b44a1881898f
Raul Martinez <raulmartinez1855@gmail.com>
2023-09-26 23:07:33 -0400

    add docstring for funcs

--- helper_methods.py
+++ helper_methods.py
@@ -1,3 +1,4 @@
+from typing import List
 from dotenv import load_dotenv
 import os
 import pandas as pd
@@ -22,42 +23,97 @@ HISTORIC_WINDOW = 30  # The number of days used to calculate the historic mean
 # Database Management
 
 
-def get_stock_prices(ticker, start_date, end_date):
-    # use date format YYYY-MM-DD
+def get_stock_prices(ticker: str, start_date: str, end_date: str) -> pd.DataFrame:
+    """
+    takes a ticker, start date, and end date. Returns a dataframe 
+    of the stock prices from yfinance.
+
+    Parameters:
+        ticker (str): the stock symbol
+        start_date (str): the start date of the date range, use date format YYYY-MM-DD
+        end_date (str): the end date of the date range, use date format YYYY-MM-DD
+
+    Returns:
+        prices_df (pd.DataFrame): returns a dataframe of the stock prices from yfinance
+    """
     prices_df = yfinance.Ticker(ticker).history(
         start=start_date, end=end_date, raise_errors=True)
     prices_df['Ticker'] = ticker
     return prices_df
 
 
-def test_stock_cointegration(stock_one, stock_two):
-    # The Null hypothesis is that there is no cointegration,
-    # the alternative hypothesis is that there is cointegrating relationship.
-    # If the pvalue is small, below a critical size, then we can reject the
-    # hypothesis that there is no cointegrating relationship.
+def test_stock_cointegration(stock_one: pd.Series, stock_two: pd.Series) -> tuple:
+    """
+    takes two pandas series of stock prices and returns a tuple of a 
+    boolean indicating if the p_value was significant and a tuple of 
+    results from the statsmodel cointegration test.
+
+    The Null hypothesis is that there is no cointegration,
+    the alternative hypothesis is that there is cointegrating relationship.
+    If the pvalue is small, below a critical size, then we can reject the
+    hypothesis that there is no cointegrating relationship.
+
+    Parameters:
+        stock_one (pd.Series): 1st pandas series of stock prices to compare cointegration
+        stock_two (pd.Series): 2nd pandas series of stock prices to compare cointegration      
+    Returns:
+        tuple
+    """
     res = coint(stock_one, stock_two)
     p_value = res[1]
     signifigance_level = .05
     return p_value < signifigance_level, res
 
 
-def adf_test(series):
-    # The null hypothesis of the Augmented Dickey-Fuller is
-    # that there is a unit root, with the alternative that
-    # there is no unit root. If the pvalue is above a critical
-    # size, then we cannot reject that there is a unit root.
+def adf_test(series: pd.Series) -> tuple:
+    """
+    takes a pandas series of stock prices and returns a tuple of a 
+    boolean indicating if the p_value was significant and a tuple of 
+    results from the statsmodel adfuller stationarity test.
+
+    The null hypothesis of the Augmented Dickey-Fuller is
+    that there is a unit root, with the alternative that
+    there is no unit root. If the pvalue is above a critical
+    size, then we cannot reject that there is a unit root.
+
+    Parameters:
+        series (pd.Series): Pandas series of stock prices to test for stationarity        
+    Returns:
+        tuple
+    """
+
     res = adfuller(series)
     p_value = res[1]
     signifigance_level = .05
     return p_value < signifigance_level, res
 
 
-def connect_with_database():
+def connect_with_database() -> psycopg2.extensions.connection:
+    """
+    this function connects to the database and returns a connection to the database
+
+    Parameters:
+        None   
+    Returns:
+        psycopg2.extensions.connection: returns a connection to the database
+    """
     db_connection = psycopg2.connect(DB_URL)
     return db_connection
 
 
-def sql_execution_wrapper(sql_statement):
+def sql_execution_wrapper(sql_statement: str) -> None:
+    """
+    this is a wrapper function to handle the execution of sql statements.
+    It will connect to the database. 
+    Execute the sql statement.
+    Commit the changes. 
+    Then close the connection.
+
+    Parameters:
+        sql_statement (str): the sql statement to execute   
+    Returns:
+        psycopg2.extensions.connection: returns a connection to the database
+    """
     conn = connect_with_database()
     cur = conn.cursor()
 
@@ -68,7 +124,15 @@ def sql_execution_wrapper(sql_statement):
     conn.close()
 
 
-def create_stock_price_table():
+def create_stock_price_table() -> None:
+    """
+    This function creates the stock_prices table in the database if it does not exist.
+
+    Parameters:
+        None 
+    Returns:
+        None
+    """
     sql_query = """
                 CREATE TABLE IF NOT EXISTS stock_prices(
                     id SERIAL PRIMARY KEY,
@@ -82,12 +146,28 @@ def create_stock_price_table():
     sql_execution_wrapper(sql_query)
 
 
-def drop_stock_cointegration_table():
+def drop_stock_cointegration_table() -> None:
+    """
+    This function deletes the stock_cointegrations table in the database if it exists.
+
+    Parameters:
+        None 
+    Returns:
+        None
+    """
     sql_query = """DROP TABLE IF EXISTS stock_cointegrations;"""
     sql_execution_wrapper(sql_query)
 
 
 def create_stock_cointegration_table() -> None:
+    """
+    This function creates the stock_cointegrations table in the database if it does not exist.
+
+    Parameters:
+        None 
+    Returns:
+        None
+    """
     sql_query = """
                 CREATE TABLE IF NOT EXISTS stock_cointegrations(
                     id SERIAL PRIMARY KEY,
@@ -102,8 +182,20 @@ def create_stock_cointegration_table() -> None:
     sql_execution_wrapper(sql_query)
 
 
-def insert_stock_coint_pairs_to_db(stock_pairs_list):
+def insert_stock_coint_pairs_to_db(stock_pairs_list: List[List]) -> None:
+    """
+    Takes list of lists from the find_cointegrated_pairs 
+    function and inserts them into the database into 
+    the stock_cointegrations table.
 
+    Parameters:
+        stock_pairs_list (List[List]): a list of lists of stock pairs 
+                                       that are cointegrated. each list contains
+                                       first stock, second stock, p_value for cointegration, 
+                                       sector, and p_value for stationarity
+    Returns:
+        None
+    """
     insert_stmt = ','.join([f'(\'{stock_one}\', \'{stock_two}\', {pvalue}, \'{sector}\',{ratio_stationarity})'
                             for stock_one, stock_two, pvalue, sector, ratio_stationarity in stock_pairs_list])
 
@@ -114,16 +206,42 @@ def insert_stock_coint_pairs_to_db(stock_pairs_list):
     sql_execution_wrapper(sql_query)
 
 
-def drop_stock_prices_table():
+def drop_stock_prices_table() -> None:
+    """
+    This function deletes the stock_prices table in the database if it exists.
+
+    Parameters:
+        None 
+    Returns:
+        None
+    """
     sql_query = """DROP TABLE IF EXISTS stock_prices;"""
     sql_execution_wrapper(sql_query)
 
 
-def find_cointegrated_pairs(df, tickers_list, sector):
-    stock_set = set()
-    pairs_list = []
-    for t1 in tickers_list:
-        for t2 in tickers_list:
+def find_cointegrated_pairs(df: pd.DataFrame, tickers: List[str], sector: str) -> List[tuple]:
+    """
+    takes a dataframe of stock prices, a list of stock symbols, and a sector. 
+    Then runs a cointegration test on each pair of stocks.
+    If the p_value is significant, then it will also conduct an 
+    adfuller test on the ratio of the two stocks. To prevent duplicates,
+    a set is used to keep track of the stock pairs that have already been tested.
+    The stocks are sorted alphabetically and then joined with a comma to create a string.
+    If the combination is new. It will be appended to the list that is returned by the function, 
+    as a list containing:
+        first stock, second stock, p_value for cointegration, sector, p_value for stationarity
+
+    Parameters:
+        df (pd.DataFrame): dataframe of stock prices
+        tickers (List[str]): list of stock symbols 
+        sector (str): the sector the tickers list of stocks belongs to
+    Returns:
+        List[List]: returns a list of list of stock pairs that are cointegrated
+    """
+    unique_pairs = set()
+    pairs = []
+    for t1 in tickers:
+        for t2 in tickers:
             if t1 == t2:
                 continue
             does_cointegrate, res = test_stock_cointegration(
@@ -132,18 +250,31 @@ def find_cointegrated_pairs(df, tickers_list, sector):
             if not does_cointegrate:
                 continue
             str_stock_pair = ','.join(list(sorted([t1, t2])))
-            curr_len = len(stock_set)
-            stock_set.add(str_stock_pair)
-            if (len(stock_set) != curr_len):
+            curr_len = len(unique_pairs)
+            unique_pairs.add(str_stock_pair)
+            if (len(unique_pairs) != curr_len):
                 ratio_stationarity = adf_test(df[t1] / df[t2])[1][1]
                 coint_pair = [*list(sorted([t1, t2])),
                               f'{round(res[1], 5)}', sector, f'{round(ratio_stationarity, 5)}']
                 print(coint_pair)
-                pairs_list.append(coint_pair)
-    return pairs_list
+                pairs.append(coint_pair)
+    return pairs
 
 
-def insert_stock_records_to_db(ticker, sector, start_date, end_date):
+def insert_stock_records_to_db(ticker: str, sector: str, start_date: str, end_date: str) -> None:
+    """
+    Orchestrates the insertion of stock prices into the database. 
+    Provides a warning if the data is not the expected length.
+    If there is an issue while trying to insert the data, it will print to the console.
+
+    Parameters:
+        ticker (str): stock symbol
+        sector (str): sector the stock belongs to
+        start_date (str): the start date, use date format YYYY-MM-DD
+        end_date (str): the end date, use date format YYYY-MM-DD
+    Returns:
+        None
+    """
     try:
         df = get_stock_prices(ticker, start_date, end_date).reset_index()
         if (len(df) != EXPECTED_DF_LEN):
@@ -163,11 +294,27 @@ def insert_stock_records_to_db(ticker, sector, start_date, end_date):
             f'Unable to find Stock Data for {ticker} in sector {sector}, skipping this entry')
 
 
-def get_stock_prices_from_db():
+def get_stock_prices_from_db() -> pd.DataFrame:
+    """
+    returns a dataframe of the stock_prices table
+
+    Parameters:
+        None
+    Returns:
+        pd.DataFrame: returns a dataframe of the stock_prices table
+    """
     return pd.read_sql_query('SELECT * FROM public.stock_prices', DB_URL)
 
 
-def get_stock_coint_pairs_from_db():
+def get_stock_coint_pairs_from_db() -> pd.DataFrame:
+    """
+    returns a dataframe of the stock_cointegrations table
+
+    Parameters:
+        None
+    Returns:
+        pd.DataFrame: returns a dataframe of the stock_prices table
+    """
     return pd.read_sql_query('SELECT * FROM public.stock_cointegrations', DB_URL)
 
 
